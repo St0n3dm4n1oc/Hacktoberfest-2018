768a3b262e8b19b085cea2849362257aca822aa9
St0n3dm4n1oc <44205234+St0n3dm4n1oc@users.noreply.github.com>
2018-10-18 19:58:39 -0300

    Update Insertionsort.c
    I tried to somehow contribute to the code, thus trying to leave the code more complete and detailed

--- C/Insertionsort.c
+++ C/Insertionsort.c
@@ -4,32 +4,32 @@
 
 int main()
 {
-  int n, array[1000], c, d, t;
+  int Value, array[1000],Counter , /*conditional accountant*/, Helper/*value taker*/;
 
-  printf("Enter number of elements\n");
-  scanf("%d", &n);
+  printf("Enter number of elements:\n");
+  scanf("%d", &Value);
 
-  printf("Enter %d integers\n", n);
+  printf("Enter %d integers\n", Value);
 
-  for (c = 0; c < n; c++)
-    scanf("%d", &array[c]);
+  for (Counter = 0; Counter < Value; Counter++)
+    scanf("%d", &array[Counter]);
 
-  for (c = 1 ; c <= n - 1; c++) {
-    d = c;
+  for (Counter = 1 ; Counter <= Value - 1; Counter++) {
+    Cond_Acc = Counter;
 
-    while ( d > 0 && array[d-1] > array[d]) {
-      t          = array[d];
-      array[d]   = array[d-1];
-      array[d-1] = t;
+    while ( Cond_Acc > 0 && array[Cond_Acc-1] > array[Cond_Acc]) {
+      Helper            = array[Cond_Acc];
+      array[Cond_Acc]   = array[Cond_Acc-1];
+      array[Cond_Acc-1] = Helper;
 
-      d--;
+      Cond_Acc--;
     }
   }
 
-  printf("Sorted list in ascending order:\n");
+  printf("The above values were ordered as follows:\n");
 
-  for (c = 0; c <= n - 1; c++) {
-    printf("%d\n", array[c]);
+  for (Counter = 0; Counter <= Value - 1; Counter++) {
+    printf("%d\n", array[Counter]);
   }
 
   return 0;
